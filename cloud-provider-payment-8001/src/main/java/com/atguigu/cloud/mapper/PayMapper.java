8b88d44c8355d6a25385db091bf505132d4823be
dong_taian <dongta@my-soft.com.cn>
2024-09-20 11:10:57 +0800

    依赖升级 javax->jakarta 去除无用的引入

--- cloud-provider-payment-8001/src/main/java/com/atguigu/cloud/mapper/PayMapper.java
+++ cloud-provider-payment-8001/src/main/java/com/atguigu/cloud/mapper/PayMapper.java
@@ -1,9 +1,7 @@
 package com.atguigu.cloud.mapper;
 
 import com.atguigu.cloud.entitys.Pay;
-import io.mybatis.mapper.BaseMapper;
 import io.mybatis.mapper.Mapper;
-import io.mybatis.mapper.logical.LogicalMapper;
 
 public interface PayMapper extends Mapper<Pay,Integer> {
 
