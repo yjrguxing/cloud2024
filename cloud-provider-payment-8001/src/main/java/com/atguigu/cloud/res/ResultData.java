400bf0321a83f88bee396e450b6809be0fa0c121
dong_taian <dongta@my-soft.com.cn>
2024-10-11 10:52:48 +0800

    优化ResultData

--- cloud-provider-payment-8001/src/main/java/com/atguigu/cloud/res/ResultData.java
+++ cloud-provider-payment-8001/src/main/java/com/atguigu/cloud/res/ResultData.java
@@ -2,7 +2,6 @@ package com.atguigu.cloud.res;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
 
@@ -32,15 +31,15 @@ public class ResultData<T> {
         this.timestamp = System.currentTimeMillis();
     }
 
-    public static<T> ResultData<T> Success(){
-        return new ResultData<>(200,"Success",null,System.currentTimeMillis());
+    public static<T> ResultData<T> success(){
+        return new ResultData<>(200,"success",null,System.currentTimeMillis());
     }
 
-    public static<T> ResultData<T> Success(T data){
-        return new ResultData<>(200,"Success",data,System.currentTimeMillis());
+    public static<T> ResultData<T> success(T data){
+        return new ResultData<>(200,"success",data,System.currentTimeMillis());
     }
 
-    public static<T> ResultData<T> Error(Integer code,String message,T data){
+    public static<T> ResultData<T> error(Integer code,String message,T data){
         return new ResultData<>(code,message,data,System.currentTimeMillis());
     }
 }
